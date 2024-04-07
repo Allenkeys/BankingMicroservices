a01fc8f64b1883465717787b9abc638fe57be975
Joshua Eze <79511167+Allenkeys@users.noreply.github.com>
2024-04-08 00:11:58 +0100

    Added IEventBus

---
+++ BankingMicroservices/AllenBank.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllenBank.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T> (T @event) where T : Event;
+        void Subscribe<TEvent, THandler> () where TEvent : Event where THandler : IEventHandler;
+    }
+}
